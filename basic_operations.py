62bf8857d2fb2ad09b30b00f28ed0d9a3af35cb8
Abyssinia Atnafe <abyssiniaqiw@gmail.com>
2024-06-23 16:57:36 -0700

    First commit

---
+++ basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+
+sum_result = number1 + number2
+print(f"Sum of {number1} and {number2} is {sum_result}")
+
+difference_result = number1 - number2
+print(f"Difference of {number1} and {number2} is {difference_result}")
+
+product_result = number1 * number2
+print(f"Product of {number1} and {number2} is {product_result}")
