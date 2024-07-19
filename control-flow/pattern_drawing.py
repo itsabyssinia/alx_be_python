52b65182caa43a7aa784dcd9359a006377f92baa
itsabyssinia <abyssiniaqiw@gmail.com>
2024-07-19 10:08:43 -0700

    Update pattern_drawing.py

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,5 +1,7 @@
 row = int(input("Enter the size of the pattern: "))
-while row > 0:
-    print("****")
-    row -= 1
-print(f"here's a {row} row pattern.")
+i = row
+while i > 0:
+    for x in range(1, row + 1):
+        print("*", end="")
+    print()
+    i -=1
